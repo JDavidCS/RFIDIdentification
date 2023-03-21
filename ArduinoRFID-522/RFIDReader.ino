f66609448c5bff36456eedd9f06fcb211df1d6ed
JDavidCS <jdcorreasierra@gmail.com>
2023-03-21 16:44:09 -0500

    build: Comunication between Node and arduino

--- ArduinoRFID-522/RFIDReader.ino
+++ ArduinoRFID-522/RFIDReader.ino
@@ -15,37 +15,80 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN  9      // Define reset pin
-#define SS_PIN  10      // Define slave select pin
+#define RST_PIN  9
+#define SS_PIN  10
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); // new object 
+#define buz 6 // passive buzzer
+
+// music tones
+#define Err  165 // if the card is not registered
+#define OK  880 // if the card is registered
+#define det 659 // when a card is detected
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // crea objeto mfrc522 enviando pines de slave select y reset
+
+byte LecturaUID[4];
 
 void setup() {
-  Serial.begin(9600);
-  SPI.begin();
-  mfrc522.PCD_Init();     // initialize module reader
-  pinMode(6, OUTPUT);
+  Serial.begin(9600);     // inicializa comunicacion por monitor serie a 9600 bps
+  SPI.begin();        // inicializa bus SPI
+  mfrc522.PCD_Init();     // inicializa modulo lector
+  pinMode(buz, OUTPUT);
 }
 
 void loop() {
-  if ( ! mfrc522.PICC_IsNewCardPresent()) // when there isn't a card
+  if ( ! mfrc522.PICC_IsNewCardPresent()) // si no hay una tarjeta presente
     return;
   
-  if ( ! mfrc522.PICC_ReadCardSerial())   // when de card cannot be read
+  if ( ! mfrc522.PICC_ReadCardSerial())   // si no puede obtener datos de la tarjeta
     return;
-    
-  for (byte i = 0; i < mfrc522.uid.size; i++) { // read and print card information
-    if (mfrc522.uid.uidByte[i] < 0x10){
-      Serial.print(" 0");
+
+  detected();
+  for (byte i = 0; i < mfrc522.uid.size; i++) { // bucle recorre de a un byte por vez el UID
+    if (mfrc522.uid.uidByte[i] < 0x10){   // si el byte leido es menor a 0x10
+      Serial.print(" 0");     // imprime espacio en blanco y numero cero
       }
-      else{ 
-      Serial.print(" ");
+      else{         // sino
+      Serial.print(" ");      // imprime un espacio en blanco
       }
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    Serial.print(mfrc522.uid.uidByte[i], HEX);  // imprime el byte del UID leido en hexadecimal
   } 
   Serial.println();
+
+  while (Serial.available() == 0) {}     //wait for data available
+  String answer = Serial.readString();  //read until timeout
+  answer.trim();
+
+  if(answer == "OK"){
+    Ok();
+  }
+  else{
+    Error();
+  }
   
-  mfrc522.PICC_HaltA();// stops card communication
+  mfrc522.PICC_HaltA();                   // detiene comunicacion con tarjeta
 
   delay(1000);
+}
+
+void detected(){
+  tone(buz, det);
+  delay(100);
+  noTone(buz);
+}
+
+void Ok(){
+  tone(buz, OK);
+  delay(620);
+  noTone(buz);
+}
+
+void Error(){
+  tone(buz, Err);
+  delay(200);
+  noTone(buz);
+  delay(20);
+  tone(buz, Err);
+  delay(400);
+  noTone(buz);
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
 
-const port = new SerialPort({ path: 'COM5', baudRate: 9600 });
+const USBport = new SerialPort({ path: 'COM6', baudRate: 9600 });
 
-
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+const parser = USBport.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
 parser.on('data', function (data) {
-    console.log('Data:', data);
+    console.log(data);
+
+    if(data == ' 49 92 FA 6E'){
+        USBport.write('OK', 'ascii');
+    }
+    else{
+        USBport.write('Err', 'ascii');
+    }
 });
