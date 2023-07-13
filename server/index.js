79d5383f8c89116457881e89e7b7d209b47e61ef
JDavidCS <jdcorreasierra@gmail.com>
2023-07-12 22:08:21 -0500

    fix: error when can't connect to the board

--- server/index.js
+++ server/index.js
@@ -7,27 +7,63 @@ const io = new Server(server);
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
 
-const USBport = new SerialPort({ path: 'COM5', baudRate: 9600 });
 
-const parser = USBport.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+const isOpen = IsOpen();
 
-io.on('connection', (socket)=>{
-    console.log('a user connected');
+isOpen
+    .then((USBport) => {
+        Open(USBport)
+    })
+    .catch( err =>{
+        Close(err);
+    })
+
+
+function IsOpen(){
+    return new Promise((resolve, reject)=>{
+        const USBport = new SerialPort({ path: 'COM5', baudRate: 9600 });
     
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        parser.removeAllListeners();
+        USBport.on('error', (err) => {
+            reject(err);
+        });
+    
+        USBport.on('open', () => {
+            resolve(USBport);
+        });
     });
+}
+
+function Open(USBport){
+    const parser = USBport.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+    
     parser.on('data', function (data) {
-        io.emit("new info", data);
         console.log(data);
         if(data == ' 49 92 FA 6E'){
             USBport.write('OK', 'ascii');
+            io.emit("new info", data);
         }
         else{
             USBport.write('Err', 'ascii');
+            io.emit("new info", "UNKNOWN");
         }
     });
-    
-    
-});
+    io.on('connection', (socket)=>{
+        console.log('a user connected');
+        
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        }); 
+    });
+}
+
+function Close(err){
+    io.on('connection', (socket)=>{
+        console.log('a user connected');
+        
+        io.emit('new info', 'ERROR, RFID reader disconnected');
+        socket.on('disconnect', () => {
+            console.log('user disconnected'); 
+        }); 
+    });
+}
+
